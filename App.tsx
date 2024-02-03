17cbd670ed88b1d84766ac4db2c829fc26780cd9
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-02-03 06:03:52 -0300

    Add SafeAreaProvider and GestureHandlerRootView

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
+    <SafeAreaProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+
     <View className='flex-1 items-center justify-center bg-slate-900'>
       <Text className='text-white'>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   );
 }
