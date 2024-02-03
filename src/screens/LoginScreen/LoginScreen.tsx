a980914c52fd73263834880613f9e1c4abcdf204
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-02-03 18:20:13 -0300

    Add background image and SVG transformer

--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { View } from 'react-native'
+import { ImageBackground, View } from 'react-native'
 
+import BG from '@assets/imgs/bg.png'
 import { EyeIcon } from 'lucide-react-native'
 
 import { Box, TextInput } from '@components'
@@ -10,17 +11,27 @@ export function LoginScreen() {
   const { colors } = themeExtracted
 
   return (
-    <Box dark>
-      <TextInput placeholder="Nome" />
+    <ImageBackground
+      source={BG}
+      style={{ flex: 1, justifyContent: 'center' }}
+      imageStyle={{ resizeMode: 'cover' }}
+    >
+      <Box>
+        <TextInput placeholder="Nome" />
 
-      <View className="h-5" />
+        <View className="h-5" />
 
-      <TextInput
-        placeholder="Senha"
-        rightComponent={
-          <EyeIcon size={24} fill={colors.gray[100]} color={colors.gray[900]} />
-        }
-      />
-    </Box>
+        <TextInput
+          placeholder="Senha"
+          rightComponent={
+            <EyeIcon
+              size={24}
+              fill={colors.gray[100]}
+              color={colors.gray[900]}
+            />
+          }
+        />
+      </Box>
+    </ImageBackground>
   )
 }
