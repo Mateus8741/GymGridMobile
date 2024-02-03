8f9cd86c36f7f7c4145f1e6ab1165524d31a9e79
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-02-03 18:21:25 -0300

    Add StatusBar component to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import { StatusBar } from 'react-native'
+
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
@@ -7,6 +9,11 @@ export default function App() {
   return (
     <SafeAreaProvider>
       <GestureHandlerRootView style={{ flex: 1 }}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
         <LoginScreen />
       </GestureHandlerRootView>
     </SafeAreaProvider>
