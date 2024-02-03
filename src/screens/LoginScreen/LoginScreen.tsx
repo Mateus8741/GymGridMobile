58a588463421f1ec7cd5929970f0e0b92a2f6684
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-02-03 17:50:09 -0300

    Update LoginScreen component with theme colors

--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -4,15 +4,23 @@ import { View } from 'react-native'
 import { EyeIcon } from 'lucide-react-native'
 
 import { Box, TextInput } from '@components'
+import { themeExtracted } from '@theme'
 
 export function LoginScreen() {
+  const { colors } = themeExtracted
+
   return (
     <Box dark>
       <TextInput placeholder="Nome" />
 
       <View className="h-5" />
 
-      <TextInput placeholder="Senha" rightComponent={<EyeIcon size={24} />} />
+      <TextInput
+        placeholder="Senha"
+        rightComponent={
+          <EyeIcon size={24} fill={colors.gray[100]} color={colors.gray[900]} />
+        }
+      />
     </Box>
   )
 }
