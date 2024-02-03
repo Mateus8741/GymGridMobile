f09b0c226dff54aa59747c793ab974e11353b3d1
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-02-03 06:20:29 -0300

    Fix linting issues and update dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
 // babel.config.js
 module.exports = function (api) {
-  api.cache(true);
+  api.cache(true)
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
       'nativewind/babel',
       [
@@ -31,5 +31,5 @@ module.exports = function (api) {
         },
       ],
     ],
-  };
-};
+  }
+}
