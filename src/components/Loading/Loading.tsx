a7e5effbbac284f3580d6b98992a424c29035fb6
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-02-04 10:48:01 -0300

    :chore: add Loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ActivityIndicator, View } from 'react-native'
+
+import { themeExtracted } from '@theme'
+
+export function Loading() {
+  const { colors } = themeExtracted
+
+  return (
+    <View className="flex-1 items-center justify-center">
+      <ActivityIndicator size="large" color={colors.lemon[300]} />
+    </View>
+  )
+}
