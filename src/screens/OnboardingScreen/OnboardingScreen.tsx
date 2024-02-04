3c0addd1652e2e86f94699c4c95da9a2f4b40bed
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-02-04 06:26:08 -0300

    Add onboarding screen with images

--- src/screens/OnboardingScreen/OnboardingScreen.tsx
+++ src/screens/OnboardingScreen/OnboardingScreen.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { FlatList, Image } from 'react-native'
+
+import { onboarding } from '@utils'
+
+import { Box } from '@components'
 
 export function OnboardingScreen() {
   return (
-    <View>
-      <Text>OnboardingScreen</Text>
-    </View>
+    <Box>
+      <FlatList
+        data={onboarding}
+        renderItem={({ item }) => (
+          <Image
+            source={item.image}
+            style={{ width: '100%', height: '100%' }}
+            alt="Onboarding Images"
+          />
+        )}
+        keyExtractor={(item) => item.id}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+      />
+    </Box>
   )
 }
---
+++ src/utils/onboarding.ts
@@ -0,0 +1,15 @@
+export const onboarding = [
+  {
+    id: '1',
+    title: 'Supere limites e alcance objetivos',
+    subtitle: 'Podemos ajudá-lo a alcançar sua melhor condição física.',
+    image: require('@assets/imgs/Onboarding-1.png'),
+  },
+  {
+    id: '2',
+    title: 'Vivencie uma melhor experiência',
+    subtitle:
+      'Acesse número de séries, repetições e evoluções de carga na Gym Buddy.',
+    image: require('@assets/imgs/Onboarding-2.png'),
+  },
+]
