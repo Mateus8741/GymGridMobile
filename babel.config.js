14bf0ded091ee58c5c940cf94f1fb0071c15e102
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-02-15 09:53:21 -0300

    Update Babel configuration and add ExpoSecureStore dependency

--- babel.config.js
+++ babel.config.js
@@ -6,9 +6,9 @@ module.exports = function (api) {
     plugins: [
       'nativewind/babel',
       'react-native-reanimated/plugin',
+      ['module:react-native-dotenv', { moduleName: '@env' }],
       [
         'module-resolver',
-        'module:react-native-dotenv',
         {
           root: '.',
           alias: {
