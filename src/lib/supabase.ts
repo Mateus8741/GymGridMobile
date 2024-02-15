e6ff6b845387798a7900c71e28cb066be48eda49
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-02-15 08:15:47 -0300

    Add expo-secure-store and @supabase/supabase-js dependencies

---
+++ src/lib/supabase.ts
@@ -0,0 +1,28 @@
+import { createClient } from '@supabase/supabase-js'
+import * as SecureStore from 'expo-secure-store'
+import 'react-native-url-polyfill/auto'
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key)
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value)
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key)
+  },
+}
+
+const supabaseUrl = 'https://arwrcnmaiswiuiuiybva.supabase.co'
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFyd3Jjbm1haXN3aXVpdWl5YnZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5Mzg1NDAsImV4cCI6MjAyMzUxNDU0MH0.E6Hr9JUEq2HTS9UiikXI9AFBD5ebizkbOnVwzhh7IgQ'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
