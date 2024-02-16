2939a76fc67670ae9a7809c44d85a26b9c6beec3
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-02-16 12:19:39 -0300

    Add theme color to GreenButton component

--- src/components/CustomButtons/GreenButton.tsx
+++ src/components/CustomButtons/GreenButton.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import * as reactNative from 'react-native'
 
+import { themeExtracted } from '@theme'
+
 interface Props extends reactNative.TouchableOpacityProps {
   title: string
   isLoading?: boolean
@@ -8,6 +10,8 @@ interface Props extends reactNative.TouchableOpacityProps {
 }
 
 export function GreenButton({ title, isLoading, isDisable, ...props }: Props) {
+  const { colors } = themeExtracted
+
   return (
     <reactNative.TouchableOpacity
       className={`w-full py-3 items-center rounded-md bg-lemon-900`}
@@ -16,7 +20,7 @@ export function GreenButton({ title, isLoading, isDisable, ...props }: Props) {
       {...props}
     >
       {isLoading ? (
-        <reactNative.ActivityIndicator size="small" color="#FFF" />
+        <reactNative.ActivityIndicator size="small" color={colors.gray.g900} />
       ) : (
         <reactNative.Text className="text-black font-700 text-base">
           {title}
