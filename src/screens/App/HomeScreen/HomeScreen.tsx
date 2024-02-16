d5981aa399429dade683ad9781f5a52b2d1291b3
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-02-16 12:14:56 -0300

    Add avatarUrl to HeaderHome component

--- src/screens/App/HomeScreen/HomeScreen.tsx
+++ src/screens/App/HomeScreen/HomeScreen.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { View } from 'react-native'
 
+import { useProfileInfo } from '@api'
+
 import { Box, Cards, HeaderHome } from '@components'
 
 export function HomeScreen() {
+  const { ProfileInfo } = useProfileInfo()
+
   return (
     <>
-      <HeaderHome />
+      <HeaderHome avatarUrl={ProfileInfo?.avatar_url || undefined} />
 
       <Box>
         <View className="flex-1 flex-row flex-wrap justify-between">
