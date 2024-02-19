d4bdfdeed247bb6e0ec645b84872d630294b0846
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-02-19 11:52:16 -0300

    Refactor HeaderText component to add navigation functionality

--- src/components/HeaderText/HeaderText.tsx
+++ src/components/HeaderText/HeaderText.tsx
@@ -1,35 +1,54 @@
 import React from 'react'
 import { Text, TouchableOpacity, View } from 'react-native'
 
-import { LogOut } from 'lucide-react-native'
+import { useNavigation } from '@react-navigation/native'
+import { ChevronLeft, LogOut } from 'lucide-react-native'
 
 import { themeExtracted } from '@theme'
 
 type HeaderTextProps = {
   title: string
   logout?: () => void
+  canGoBack?: boolean
 }
 
-export function HeaderText({ title, logout }: HeaderTextProps) {
+export function HeaderText({ title, canGoBack, logout }: HeaderTextProps) {
   const { colors } = themeExtracted
 
-  const place = logout ? 'justify-between' : 'justify-center'
+  const { goBack } = useNavigation()
+
+  const place = logout || canGoBack ? 'justify-between' : 'justify-center'
+
+  function handleGoBack() {
+    goBack()
+  }
 
   return (
     <View className={`flex-row mt-6 items-center ${place}`}>
-      {logout && <View className="w-6 h-6" />}
+      {canGoBack ? (
+        <TouchableOpacity
+          className="w-6 h-6 p-4 rounded-md items-center justify-center"
+          onPress={handleGoBack}
+        >
+          <ChevronLeft size={24} color={colors.lemon[300]} />
+        </TouchableOpacity>
+      ) : (
+        <View className="w-6 h-6" />
+      )}
 
       <Text className="text-white uppercase text-lg text-center font-700">
         {title}
       </Text>
 
-      {logout && (
+      {logout ? (
         <TouchableOpacity
           className="bg-red-600/30 w-6 h-6 p-4 rounded-md items-center justify-center"
           onPress={logout}
         >
           <LogOut size={20} color={colors.danger[400]} />
         </TouchableOpacity>
+      ) : (
+        <View className="w-6 h-6" />
       )}
     </View>
   )
