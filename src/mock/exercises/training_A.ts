2459f7ebd4802161fc1005ce9f152a8c254ff227
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-02-20 13:27:22 -0300

    Add mock exercise data for training A

---
+++ src/mock/exercises/training_A.ts
@@ -0,0 +1,67 @@
+import { ExerciseCardsProps } from '@components'
+
+export const $trainingA: ExerciseCardsProps[] = [
+  {
+    item: {
+      imageUrl: 'https://via.placeholder.com/150',
+      title: 'Supino',
+      description: '3 séries de 15 repetições',
+    },
+  },
+  {
+    item: {
+      imageUrl: 'https://via.placeholder.com/150',
+      title: 'Supino Inclinado',
+      description: '3 séries de 15 repetições',
+    },
+  },
+  {
+    item: {
+      imageUrl: 'https://via.placeholder.com/150',
+      title: 'Supino Declinado',
+      description: '3 séries de 15 repetições',
+    },
+  },
+  {
+    item: {
+      imageUrl: 'https://via.placeholder.com/150',
+      title: 'Rosca Direta',
+      description: '3 séries de 15 repetições',
+    },
+  },
+  {
+    item: {
+      imageUrl: 'https://via.placeholder.com/150',
+      title: 'Rosca Alternada',
+      description: '3 séries de 15 repetições',
+    },
+  },
+  {
+    item: {
+      imageUrl: 'https://via.placeholder.com/150',
+      title: 'Rosca Martelo',
+      description: '3 séries de 15 repetições',
+    },
+  },
+  {
+    item: {
+      imageUrl: 'https://via.placeholder.com/150',
+      title: 'Tríceps Testa',
+      description: '3 séries de 15 repetições',
+    },
+  },
+  {
+    item: {
+      imageUrl: 'https://via.placeholder.com/150',
+      title: 'Tríceps Pulley',
+      description: '3 séries de 15 repetições',
+    },
+  },
+  {
+    item: {
+      imageUrl: 'https://via.placeholder.com/150',
+      title: 'Tríceps Corda',
+      description: '3 séries de 15 repetições',
+    },
+  },
+]
---
+++ src/mock/index.ts
@@ -0,0 +1,3 @@
+/* eslint-disable prettier/prettier */
+export * from './exercises/training_A';
+
