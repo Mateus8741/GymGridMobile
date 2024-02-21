aedfeefae7623c87c01ea61258532a0f8d4903d8
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-02-21 11:20:22 -0300

    Add ExerciseDetailsScreen component

---
+++ src/screens/App/ExerciseDetailsScreen/ExerciseDetailsScreen.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+import { Box, HeaderText } from '@components'
+import { AppScreenProps } from '@routes'
+
+export function ExerciseDetailsScreen({
+  route,
+}: AppScreenProps<'ExerciseDetailsScreen'>) {
+  const { card } = route.params
+
+  console.log('card', card)
+
+  return (
+    <Box>
+      <HeaderText title={card.item.title} canGoBack />
+
+      <Text>ExerciseDetailsScreen</Text>
+    </Box>
+  )
+}
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -3,6 +3,7 @@ export * from './Auth/LoginScreen/LoginScreen'
 export * from './Auth/OnboardingScreen/OnboardingScreen'
 
 export * from './App/EvolutionScreen/EvolutionScreen'
+export * from './App/ExerciseDetailsScreen/ExerciseDetailsScreen'
 export * from './App/ExerciseListScreen/ExerciseListScreen'
 export * from './App/HistoryScreen/HistoryScreen'
 export * from './App/HomeScreen/HomeScreen'
